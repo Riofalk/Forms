e48aa1871673e71c9862bb8c3f9ca78ba06e32f7
Nikita Bogomazov <riofalk@gmail.com>
2022-09-20 18:08:51 +0300

    User model created

---
+++ back-end/models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema( {
+    userName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    respondedIn: {
+        type: []
+    },
+    formCreator: {
+        type: []
+    }
+    }, {timestamps: true});
+
+export default mongoose.model('user', userSchema)
